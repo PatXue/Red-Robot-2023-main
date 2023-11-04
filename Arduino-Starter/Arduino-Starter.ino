c4294ed05d02e5dfdb64658e80fe7caaea3125a7
PatXue <95881915+PatXue@users.noreply.github.com>
2023-11-04 18:25:36 -0400

    Add a bunch of stuff
    Currently debugging hardware issue on robot

--- Arduino-Starter/Arduino-Starter.ino
+++ Arduino-Starter/Arduino-Starter.ino
@@ -12,108 +12,128 @@
 QTRSensors qtr;
 
 const uint8_t SensorCount = 6;
-uint16_t sensorValues[SensorCount];
+int sensorValues[SensorCount];
+
+// PID control loop values
+const float Kp = 0.5;
+const float Ki = 0.0;
+const float Kd = 0.1;
+float accum = 0.0;
+float prevError = 0.0;
+const float speed = 0.5;
+
+bool auton_mode = false;
+
+int servo2Rot = 90;
+
+float blackLinePos(int sensorValues[SensorCount]) {
+  int n = 0;
+  int sum = 0;
+
+  // Calculate minimum intensity in sensorValues
+  int min = sensorValues[0];
+  for (int i = 0; i < SensorCount; i++) {
+    if (sensorValues[i] < min) min = sensorValues[i];
+  }
+
+  for (int i = 0; i < SensorCount; i++) {
+    sum += i * (sensorValues[i] - min);
+    n += sensorValues[i] - min;
+  }
+  // Return average index of values in sensorValues
+  return sum / (1.0 * n);
+}
+
+void setMotors(float speed, float rot) {
+  RR_setMotor1(speed - rot);
+  RR_setMotor2(speed + rot);
+}
 
 void setup() {
   Serial.begin(115200);
+
+  RR_setServo2(servo2Rot);
+
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 int temp = 0;
 
 void loop() {
-  // Read the four joystick axes
-  // These will be in the range [-1.0, 1.0]
-  float rightX = RR_axisRX();
-  float rightY = RR_axisRY();
-  float leftX  = RR_axisLX();
-  float leftY  = RR_axisLY();
+  RR_setMotor4(1.0);
+  RR_setMotor2(1.0);
+  return;
+  // Get start and back buttons to switch between autonomous and manual modes
+  bool btnStart = RR_buttonStart();
+  bool btnBack = RR_buttonBack();
 
-  // Arcade-drive scheme
-  // Left Y-axis = throttle
-  // Right X-axis = steering
-  RR_setMotor1(leftY + rightX);
-  RR_setMotor2(leftY - rightX);
-
-  // Get the button states
-  bool btnA = RR_buttonA();
-  bool btnB = RR_buttonB();
-  bool btnX = RR_buttonX();
-  bool btnY = RR_buttonY();
-  bool btnRB = RR_buttonRB();
-  bool btnLB = RR_buttonLB();
-
-  // Control motor3 port (unused on base robot) using A/B buttons
-  if (btnA) {
-    RR_setMotor3(1.0);
+  if (btnStart) {
+    auton_mode = true;
+    digitalWrite(LED_BUILTIN, HIGH);
   }
-  else if (btnB) {
-    RR_setMotor3(-1.0);
-  }
-  else {
-    RR_setMotor3(0.0);
+  else if (btnBack) {
+    auton_mode = false;
+    digitalWrite(LED_BUILTIN, LOW);
   }
 
-  // Control motor4 port (unused on base robot) using X/Y buttons
-  if (btnX) {
-    RR_setMotor4(1.0);
+  if (!auton_mode) {
+    // Read the four joystick axes
+    // These will be in the range [-1.0, 1.0]
+    float rightX = RR_axisRX();
+    float rightY = RR_axisRY();
+    float leftX  = RR_axisLX();
+    float leftY  = RR_axisLY();
+
+    // Get the button states
+    bool btnA = RR_buttonA();
+    bool btnB = RR_buttonB();
+    bool btnX = RR_buttonX();
+    bool btnY = RR_buttonY();
+    bool btnRB = RR_buttonRB();
+    bool btnLB = RR_buttonLB();
+    bool btnRT = RR_buttonRT();
+    bool btnLT = RR_buttonLT();
+
+    // Motor sensitivity
+    float k = btnRT ? 0.25 : 1.0;
+
+    // Arcade-drive scheme
+    // Left Y-axis = throttle
+    // Right X-axis = steering
+    setMotors(k * leftY, k * rightX);
+
+    // Control servo 2 using the shoulder buttons
+    if (btnLB && servo2Rot < 135) servo2Rot += 2;
+    else if (btnRB && servo2Rot > 45) servo2Rot -= 2;
+    RR_setServo2(servo2Rot);
+
+    // we also have RR_setServo3 and RR_setServo4 available
   }
-  else if (btnY) {
-    RR_setMotor4(-1.0);
+  else if (auton_mode) {
+    // read the ultrasonic sensors
+    // Serial.print("Ultrasonic=");
+    // Serial.print(RR_getUltrasonic());
+    // Serial.print(" ;; ");
+    // int sensors[6];
+
+    // Serial.print("Line sensors=");
+    // RR_getLineSensors(sensorValues);
+    // for (int i = 0; i < 6; ++i) {
+    //   Serial.print(sensorValues[i]);
+    //   Serial.print(" ");
+    // }
+    // Serial.print("\tLine pos=");
+    // Serial.print(blackLinePos(sensorValues));
+    // Serial.println();
+
+    float error = blackLinePos(sensorValues) - 2.5;
+    accum += error * 0.02;
+    float deriv = (error - prevError) / 0.02;
+    float adjustment = Kp*error + Ki*accum + Kd*deriv;
+    prevError = error;
+
+    setMotors(speed, adjustment*speed);
   }
-  else {
-    RR_setMotor4(0.0);
-  }
-
-  // Control servo 1 using the dpad
-  // 6 = left, 2 = right, 0 = up, 4 = down, 8 = center
-  // (note that you will also see the value 0 if the controller
-  //  is disconnected)
-  if (RR_dpad() == 6) { // left
-
-    // we can't move a servo less than 0 degrees
-    if (temp > 0) temp -= 10;
-  }
-  else if (RR_dpad() == 2) { // right
-
-    // we can't move a servo past 180 degrees
-    // for continuous rotation, try using a DC motor
-    if (temp < 180) temp += 10;
-  }
-  RR_setServo1(temp);
-
-  // Control servo 2 using the shoulder buttons
-  // This example moves the servo to fixed points
-  // You can change the angles based on your mechanism
-  // (this is great for a mechanism that only has 2 states,
-  //  such as a grabber or hook)
-  if (btnRB) {
-    RR_setServo2(180);
-  }
-  else if (btnLB) {
-    RR_setServo2(0);
-  }
-
-  // we also have RR_setServo3 and RR_setServo4 available
-
-
-  // read the ultrasonic sensors
-
-  Serial.print("Ultrasonic=");
-  Serial.print(RR_getUltrasonic());
-  Serial.print(" ;; ");
-  int sensors[6];
-
-  Serial.print("Line sensors=");
-  RR_getLineSensors(sensors);
-  for (int i = 0; i < 6; ++i) {
-    Serial.print(sensors[i]);
-    Serial.print(" ");
-  }
-  Serial.print(btnA ? 1 : 0);
-  Serial.print(btnB ? 1 : 0);
-  Serial.print(btnX ? 1 : 0);
-  Serial.print(btnY ? 1 : 0);
-  Serial.println();
 
   // This is important - it sleeps for 0.02 seconds (= 50 times / second)
   // Running the code too fast will overwhelm the microcontroller and peripherals
